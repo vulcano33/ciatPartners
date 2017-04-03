a2b7656810429597cf34129d111bd49e9f3426a8
Vulcano33 <c.cabrera.camacho@gmail.com>
2017-04-03 14:53:07 -0500

    Add some additional validations

--- src/main/java/org/cgiar/ciat/action/RegisterAction.java
+++ src/main/java/org/cgiar/ciat/action/RegisterAction.java
@@ -31,7 +31,7 @@ public class RegisterAction extends ActionSupport {
 	 * Institution to be saved
 	 */
 	private Institution institution = null;
-	
+
 	/**
 	 * Option for the radio button in savePartner view
 	 */
@@ -41,22 +41,22 @@ public class RegisterAction extends ActionSupport {
 	 * Option for the radio button in savePartner view
 	 */
 	private static final String NO = "No";
-	
+
 	/**
 	 * List of options available
 	 */
 	private ArrayList<String> options = null;
-	
+
 	/**
 	 * List of headquarter institutions available
 	 */
 	private ArrayList<String> institutionsList = null;
-	
+
 	/**
 	 * List of institution types
 	 */
 	private ArrayList<String> institutionTypes = null;
-	
+
 	/**
 	 * Option for the institution types control
 	 */
@@ -76,7 +76,7 @@ public class RegisterAction extends ActionSupport {
 	 * Option for the institution types control
 	 */
 	private static final String INSTITUTION_TYPE_4 = "Research Institution";
-	
+
 	/**
 	 * List of countries
 	 */
@@ -84,16 +84,17 @@ public class RegisterAction extends ActionSupport {
 
 	/**
 	 * Default constructor
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	public RegisterAction() throws IOException {
-		
+
 		// If all is correct, return showList, in other case return save
 		// Add options for the radio control
 		options = new ArrayList<>();
 		options.add(YES);
 		options.add(NO);
-		
+
 		// Add headquarter institutions list
 		institutionsList = new ArrayList<>();
 		ArrayList<Institution> institutions = InstitutionService.getInstance().readInstitutions();
@@ -118,11 +119,46 @@ public class RegisterAction extends ActionSupport {
 	@Override
 	public String execute() throws JsonGenerationException, JsonMappingException, IOException {
 
+		// Verify that there are no request with the same name or acronym
+		ArrayList<Institution> institutions = InstitutionService.getInstance().readInstitutions();
+
+		for (Institution institution : institutions) {
+
+			// Verify if the institution request to be registered is a branch
+			if (this.institution.getBranch() != null && this.institution.getBranch().equalsIgnoreCase("Yes")) {
+				if (this.institution.getCountry() != null
+						&& this.institution.getCountry().equals(institution.getCountry())) {
+					addFieldError(institution.getName(),
+							"Sorry, the institution branch must be located in a different country");
+					return "input";
+				} else if (this.institution.getCity() != null
+						&& this.institution.getCity().equals(institution.getCity())) {
+					addFieldError(institution.getName(),
+							"Sorry, the institution branch must be located in a different city");
+					return "input";
+				}
+			} else {
+
+				// The institution to be registered is a headquarter, so names
+				// must be validated
+				if (institution.getName().equals(this.institution.getName())) {
+					addFieldError(institution.getName(), "Sorry, the institution name already exist");
+					return "input";
+				}
+
+				if (institution.getAcronym().equals(this.institution.getAcronym())) {
+
+					addFieldError(institution.getName(), "Sorry, the institution acronym already exist");
+					return "input";
+				}
+			}
+		}
+
 		// Save institution
 		InstitutionService.getInstance().saveInstitution(institution);
-		
+
 		addActionMessage("The request for a new institution has been succesfully created");
-		
+
 		return "success";
 	}
 
@@ -144,74 +180,86 @@ public class RegisterAction extends ActionSupport {
 	public void setInstitution(Institution institution) {
 		this.institution = institution;
 	}
-	
+
 	/**
-	 * Get method for options 
+	 * Get method for options
+	 * 
 	 * @return the options
 	 */
 	public ArrayList<String> getOptions() {
 		return options;
 	}
-	
+
 	/**
 	 * Set method for options
-	 * @param options the options to set
+	 * 
+	 * @param options
+	 *            the options to set
 	 */
 	public void setOptions(ArrayList<String> options) {
 		this.options = options;
 	}
-	
+
 	/**
-	 * Get method for institutionsList 
+	 * Get method for institutionsList
+	 * 
 	 * @return the institutionsList
 	 */
 	public ArrayList<String> getInstitutionsList() {
 		return institutionsList;
 	}
-	
+
 	/**
 	 * Set method for institutionsList
-	 * @param institutionsList the institutionsList to set
+	 * 
+	 * @param institutionsList
+	 *            the institutionsList to set
 	 */
 	public void setInstitutionsList(ArrayList<String> institutionsList) {
 		this.institutionsList = institutionsList;
 	}
-	
+
 	/**
-	 * Get method for institutionTypes 
+	 * Get method for institutionTypes
+	 * 
 	 * @return the institutionTypes
 	 */
 	public ArrayList<String> getInstitutionTypes() {
 		return institutionTypes;
 	}
-	
+
 	/**
 	 * Set method for institutionTypes
-	 * @param institutionTypes the institutionTypes to set
+	 * 
+	 * @param institutionTypes
+	 *            the institutionTypes to set
 	 */
 	public void setInstitutionTypes(ArrayList<String> institutionTypes) {
 		this.institutionTypes = institutionTypes;
 	}
-	
+
 	/**
-	 * Get method for countries 
+	 * Get method for countries
+	 * 
 	 * @return the countries
 	 */
 	public ArrayList<String> getCountries() {
 		return countries;
 	}
-	
+
 	/**
 	 * Set method for countries
-	 * @param countries the countries to set
+	 * 
+	 * @param countries
+	 *            the countries to set
 	 */
 	public void setCountries(ArrayList<String> countries) {
 		this.countries = countries;
 	}
-	
-	/* 
-	 * Method for form validations
-	 * (non-Javadoc)
+
+	/*
+	 * Method for form validations (non-Javadoc)
+	 * 
 	 * @see com.opensymphony.xwork2.ActionSupport#validate()
 	 */
 	public void validate() {
